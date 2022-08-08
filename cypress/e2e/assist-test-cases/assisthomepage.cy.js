1954b3ed835f75f84217037ad9cb340fb4eac837
AlinaBalinisteanu <109555449+AlinaBalinisteanu@users.noreply.github.com>
2022-08-08 23:46:51 +0300

    Automated-Tests
    Cypress-Basic Automated tests

---
+++ cypress/e2e/assist-test-cases/assisthomepage.cy.js
@@ -0,0 +1,6 @@
+describe('Visit ASSIST SOFTWARE SITE', () => {
+    it('Go to the Assist Software Home page', () => {
+      cy.visit('https://assist-devqa.netlify.app')
+  });
+  })
+
---
+++ cypress/e2e/assist-test-cases/test-assist.cy.js
@@ -0,0 +1,9 @@
+/// <reference types = "Cypress" />
+
+describe('Visit ASSIST SOFTWARE SITE', () => {
+    it('Go to the Assist Software Home page', () => {
+      cy.visit('https://assist-software.net/')
+  });
+})
+
+  
